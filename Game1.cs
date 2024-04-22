5c28a8013761c9890e8100081fc75f73cd7c63fd
Ethan Jones <339467243@GoTVDSB.ca>
2024-04-22 13:06:55 -0400

    apr 22

--- Game1.cs
+++ Game1.cs
@@ -8,7 +8,10 @@ namespace Animation
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        Texture2D greyTribble;
+        Rectangle window;
+        Rectangle greyTribbleRect;
+        Vector2 greyTribbleSpeed;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,14 +22,19 @@ namespace Animation
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            window = new Rectangle(0, 0, 800,600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+            greyTribbleRect = new Rectangle(300,10,100,100);
+            greyTribbleSpeed = new Vector2(1,1);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            greyTribble = Content.Load<Texture2D>("tribbleGrey");
             // TODO: use this.Content to load your game content here
         }
 
@@ -36,16 +44,27 @@ namespace Animation
                 Exit();
 
             // TODO: Add your update logic here
-
+            greyTribbleRect.X += (int)greyTribbleSpeed.X;
+            greyTribbleRect.Y += (int)greyTribbleSpeed.Y;
             base.Update(gameTime);
+            if (greyTribbleRect.Right > window.Width || greyTribbleRect.Left < 0)
+            {
+                greyTribbleSpeed.X = greyTribbleSpeed.X * -1;
+            }
+            if (greyTribbleRect.Bottom > window.Height || greyTribbleRect.Top < 0)
+            {
+                greyTribbleSpeed.Y = greyTribbleSpeed.Y * -1;
+            }
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.LightPink);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(greyTribble, greyTribbleRect, Color.White);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
